f6fd39be35fbff9401d50a12e333147499484ecf
Axel Briano <54673215+axelbrians@users.noreply.github.com>
2024-03-19 20:43:04 +0700

    [Feat][Android] Use CardLandscapeHorizontal on MainActivity

--- composeApp/src/androidMain/kotlin/org/axelbrians/themovies/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/axelbrians/themovies/MainActivity.kt
@@ -4,15 +4,33 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import composeable.CardLandscapeHorizontal
+import theme.AppThemeScaffold
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Box(modifier = Modifier.fillMaxSize()) {
+                App()
+                AppThemeScaffold {
+                    CardLandscapeHorizontal(
+                        modifier = Modifier.align(Alignment.BottomCenter)
+                    ) {
+                        this.ContentTitleSubtitle(
+                            title = "Mixed Doubles: Tan Kian",
+                            subtitle = "SPOTV"
+                        )
+                    }
+                }
+            }
         }
     }
 }
